9bf89a387ac8364f15fb8f8f1be1145e2f30e6d6
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-19 10:15:15 +0200

    add var Quincy

--- Quincy.cpp
+++ Quincy.cpp
@@ -3,4 +3,13 @@
 #include <cstdlib>
 #include <ctime>
 
-
+Quincy::Quincy(std::string nom) : Personnage(nom, 1000)
+{
+    srand(time(0));
+    heilingPfeilDegats = rand() % 500 + 500;
+    gritzDegats = rand() % 300 + 300;
+    sklavetiDegats = rand() % 700 + 700;
+    tempsRechargeHeilingPfeil = rand() % 4 + 1;
+    tempsRechargeGritz = rand() % 4 + 1;
+    tempsRechargeSklaveti = rand() % 4 + 1;
+}
