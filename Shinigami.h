1a5b87cf64ece584ec7c5a64b0b7c7ed27fa1762
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-19 09:54:26 +0200

    add class Shinigami

--- Shinigami.h
+++ Shinigami.h
@@ -3,6 +3,23 @@
 
 #include "Personnage.hpp"
 
+class Shinigami : public Personnage
+{
+    private:
+        int zanpakutoDegats;
+        int shikaieDegats;
+        int bankaiDegats;
+        int tempsRechargeZanpakuto;
+        int tempsRechargeShikaie;
+        int tempsRechargeBankai;
 
+    public:
+        Shinigami(std::string nom);
+        void attaquer(Personnage* cible, int degats); 
+        void recevoirDegats(int degats);
+        void utiliserZanpakuto(Personnage* cible);
+        void utiliserShikaie(Personnage* cible);
+        void utiliserBankai(Personnage* cible);
+};
 
 #endif
