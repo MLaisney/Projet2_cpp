a4f0ca8ed73f7b1bac519bfc418f1b17d86e8032
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-19 10:33:47 +0200

    add comment

--- main.cpp
+++ main.cpp
@@ -12,14 +12,17 @@ int main()
 
     std::string nomShinigami, nomUryu, nomJugram;
 
+    // Choix du nom de son personnage
     std::cout << "Nom du Shinigami : ";
     std::cin >> nomShinigami;
 
     Shinigami shinigami(nomShinigami);
 
+    // Création des personnages ennemis
     Quincy uryu("Uryu Ishida");
     Quincy jugram("Jugram Haschwalth");
 
+    // Initialisation des équipes
     std::vector<Personnage*> equipeShinigamis = {&shinigami};
     std::vector<Personnage*> equipeQuincys = {&uryu, &jugram};
 }
