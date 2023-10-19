5f845037594d7c0a504ba8255d88e0ccc871c4e8
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-19 10:06:04 +0200

    cap attributs

--- Shinigami.cpp
+++ Shinigami.cpp
@@ -3,3 +3,14 @@
 #include <cstdlib>
 #include <ctime>
 
+Shinigami::Shinigami(std::string nom) : Personnage(nom, 2000)
+{
+    srand(time(0));
+    zanpakutoDegats = rand() % 500 + 500;
+    shikaieDegats = rand() % 300 + 300;
+    bankaiDegats = rand() % 700 + 700;
+    tempsRechargeZanpakuto = rand() % 4 + 1;
+    tempsRechargeShikaie = rand() % 4 + 1;
+    tempsRechargeBankai = rand() % 4 + 1;
+}
+
