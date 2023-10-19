c757fd178335f777f8f19bdc28ca3bd2b5091a44
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-19 10:13:47 +0200

    add comment

--- Personnage.cpp
+++ Personnage.cpp
@@ -3,11 +3,13 @@
 Personnage::Personnage(std::string nom, int pointsDeVie) :
 nom(nom), pointsDeVie(pointsDeVie){}
 
+// Fonction pour le retour des points de vie
 bool Personnage::estEnVie()
 {
     return pointsDeVie > 0;
 }
 
+// Fonction pour initialisation du nom du personnage
 std::string Personnage::getnom()
 {
     return nom;
