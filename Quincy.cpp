08fa363b6106ac775c2fce1e75e10aa860667256
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-19 10:16:21 +0200

    add Quincy attaque

--- Quincy.cpp
+++ Quincy.cpp
@@ -13,3 +13,16 @@ Quincy::Quincy(std::string nom) : Personnage(nom, 1000)
     tempsRechargeGritz = rand() % 4 + 1;
     tempsRechargeSklaveti = rand() % 4 + 1;
 }
+
+void Quincy::attaquer(Personnage* cible)
+{
+    int capacite = rand() % 3 + 1; // Choix aléatoire de la capacité (entre 1 et 3)
+    
+    switch (capacite)
+    {
+        case 1: utiliserHeilingPfeil(cible); break;
+        case 2: utiliserGritz(cible); break;
+        case 3: utiliserSklaveti(cible); break;
+        default: std::cout << "Capacité invalide.\n";
+    }
+}
